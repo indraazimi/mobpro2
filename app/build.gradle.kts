b7ea712c2ec90daed961849701fe55f9b6515feb
Indra Azimi <indraazimi@yahoo.com>
2023-09-24 21:09:17 +0700

    Menambahkan informasi copyright

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2020-2023 Indra Azimi. All rights reserved.
+ *
+ * Dibuat untuk kelas Pemrograman untuk Perangkat Bergerak 2.
+ * Dilarang melakukan penggandaan dan atau komersialisasi,
+ * sebagian atau seluruh bagian, baik cetak maupun elektronik
+ * terhadap project ini tanpa izin pemilik hak cipta.
+ */
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
