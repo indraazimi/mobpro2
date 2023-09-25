indraazimi/covid-indonesia

Melakukan request data ke API dengan Retrofit

app/src/main/java/com/indraazimi/covid19id/MainViewModel.kt
@@ -1,0 +1,38 @@
+/*
+ * Copyright (c) 2020 Indra Azimi. All rights reserved.
+ *
+ * Dibuat untuk kelas Pemrograman untuk Perangkat Bergerak 2.
+ * Dilarang melakukan penggandaan dan atau komersialisasi,
+ * sebagian atau seluruh bagian, baik cetak maupun elektronik
+ * terhadap project ini tanpa izin pemilik hak cipta.
+ */
+
+package com.indraazimi.covid19id
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class MainViewModel : ViewModel() {
+
+    init {
+        viewModelScope.launch {
+            withContext(Dispatchers.IO) {
+                requestData()
+            }
+        }
+    }
+
+    private suspend fun requestData() {
+        try {
+            val result = Covid19Api.service.getData()
+            Log.d("REQUEST", result.substring(0, 100))
+        }
+        catch (e: Exception) {
+            Log.d("REQUEST", e.message.toString())
+        }
+    }
+}

indraazimi/mobpro2

app/src/main/java/com/indraazimi/mobpro2/MainViewModel.kt
@@ -1,0 +1,38 @@
+/*
+ * Copyright (c) 2020-2023 Indra Azimi. All rights reserved.
+ *
+ * Dibuat untuk kelas Pemrograman untuk Perangkat Bergerak 2.
+ * Dilarang melakukan penggandaan dan atau komersialisasi,
+ * sebagian atau seluruh bagian, baik cetak maupun elektronik
+ * terhadap project ini tanpa izin pemilik hak cipta.
+ */
+
+package com.indraazimi.mobpro2
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class MainViewModel : ViewModel() {
+
+    init {
+        viewModelScope.launch {
+            withContext(Dispatchers.IO) {
+                requestData()
+            }
+        }
+    }
+
+    private suspend fun requestData() {
+        try {
+            val result = Covid19Api.service.getData()
+            Log.d("REQUEST", result.substring(0, 100))
+        }
+        catch (e: Exception) {
+            Log.d("REQUEST", e.message.toString())
+        }
+    }
+}