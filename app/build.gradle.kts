7fbab560570e29c785730870890ccbee89275d59
Indra Azimi <indraazimi@yahoo.com>
2023-10-02 11:24:47 +0700

    Menambahkan Firebase ke dalam project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,6 +10,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -51,6 +52,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
