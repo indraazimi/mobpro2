indraazimi/login-firebase

Update UI berdasarkan status login

app/src/main/java/com/indraazimi/login/FirebaseUserLiveData.kt
@@ -1,0 +1,31 @@
+/*
+ * Copyright (c) 2020 Indra Azimi. All rights reserved.
+ *
+ * Dibuat untuk kelas Pemrograman untuk Perangkat Bergerak 2.
+ * Dilarang melakukan penggandaan dan atau komersialisasi,
+ * sebagian atau seluruh bagian, baik cetak maupun elektronik
+ * terhadap project ini tanpa izin pemilik hak cipta.
+ */
+
+package com.indraazimi.login
+
+import androidx.lifecycle.LiveData
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+
+class FirebaseUserLiveData : LiveData<FirebaseUser?>() {
+
+    private val firebaseAuth = FirebaseAuth.getInstance()
+
+    private val authStateListener = FirebaseAuth.AuthStateListener { firebaseAuth ->
+        value = firebaseAuth.currentUser
+    }
+
+    override fun onActive() {
+        firebaseAuth.addAuthStateListener(authStateListener)
+    }
+
+    override fun onInactive() {
+        firebaseAuth.removeAuthStateListener(authStateListener)
+    }
+}

indraazimi/mobpro2

app/src/main/java/com/indraazimi/mobpro2/FirebaseUserLiveData.kt
@@ -1,0 +1,31 @@
+/*
+ * Copyright (c) 2020-2023 Indra Azimi. All rights reserved.
+ *
+ * Dibuat untuk kelas Pemrograman untuk Perangkat Bergerak 2.
+ * Dilarang melakukan penggandaan dan atau komersialisasi,
+ * sebagian atau seluruh bagian, baik cetak maupun elektronik
+ * terhadap project ini tanpa izin pemilik hak cipta.
+ */
+
+package com.indraazimi.mobpro2
+
+import androidx.lifecycle.LiveData
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+
+class FirebaseUserLiveData : LiveData<FirebaseUser?>() {
+
+    private val firebaseAuth = FirebaseAuth.getInstance()
+
+    private val authStateListener = FirebaseAuth.AuthStateListener { firebaseAuth ->
+        value = firebaseAuth.currentUser
+    }
+
+    override fun onActive() {
+        firebaseAuth.addAuthStateListener(authStateListener)
+    }
+
+    override fun onInactive() {
+        firebaseAuth.removeAuthStateListener(authStateListener)
+    }
+}