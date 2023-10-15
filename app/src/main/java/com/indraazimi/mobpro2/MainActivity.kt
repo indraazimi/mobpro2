jonrysimbolon/mahasiswa-id

Mengambil data mahasiswa dari dialog

app/src/main/java/com/indraazimi/mahasiswaid/MainActivity.kt
@@ -12,6 +12,7 @@ package com.indraazimi.mahasiswaid
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.indraazimi.mahasiswaid.data.Mahasiswa
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), MainDialog.DialogListener {
@@ -25,7 +26,7 @@ class MainActivity : AppCompatActivity(), MainDialog.DialogListener {
         }
     }
 
-    override fun processDialog() {
-        Log.d("MainActivity", "Dialog button clicked!")
+    override fun processDialog(mahasiswa: Mahasiswa) {
+        Log.d("MainActivity", mahasiswa.toString())
     }
 }

app/src/main/java/com/indraazimi/mahasiswaid/MainDialog.kt
@@ -11,9 +11,14 @@ package com.indraazimi.mahasiswaid
 
 import android.app.Dialog
 import android.os.Bundle
+import android.view.Gravity
 import android.view.LayoutInflater
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
+import com.indraazimi.mahasiswaid.data.Mahasiswa
 
 class MainDialog : DialogFragment() {
 
@@ -26,15 +31,48 @@ class MainDialog : DialogFragment() {
             setTitle(R.string.mahasiswa_baru)
             setView(view)
             setPositiveButton(R.string.simpan) { _, _ ->
+                val mahasiswa = getData(view) ?: return@setPositiveButton
                 val listener = requireActivity() as DialogListener
-                listener.processDialog()
+                listener.processDialog(mahasiswa)
             }
             setNegativeButton(R.string.batal) { _, _ -> dismiss() }
         }
         return builder.create()
     }
 
+    private fun getData(view: View): Mahasiswa? {
+        val nimEditText = view.findViewById<EditText>(R.id.nimEditText)
+        val namaEditText = view.findViewById<EditText>(R.id.namaEditText)
+
+        // Lakukan sanity check terlebih dahulu
+        if (nimEditText.text.isEmpty()) {
+            showMessage(R.string.nim_wajib_diisi)
+            return null
+        }
+
+        if (nimEditText.text.length != 10) {
+            showMessage(R.string.nim_harus_10chars)
+            return null
+        }
+
+        if (namaEditText.text.isEmpty()) {
+            showMessage(R.string.nama_wajib_diisi)
+            return null
+        }
+
+        return Mahasiswa(
+            nim = nimEditText.text.toString(),
+            nama = namaEditText.text.toString()
+        )
+    }
+
+    private fun showMessage(messageResId: Int) {
+        val toast = Toast.makeText(requireContext(), messageResId, Toast.LENGTH_LONG)
+        toast.setGravity(Gravity.CENTER, 0, 0)
+        toast.show()
+    }
+
     interface DialogListener {
-        fun processDialog()
+        fun processDialog(mahasiswa: Mahasiswa)
     }
 }

indraazimi/mobpro2

app/src/main/java/com/indraazimi/mobpro2/MainActivity.kt
@@ -12,6 +12,7 @@ package com.indraazimi.mobpro2
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.indraazimi.mobpro2.data.Mahasiswa
 import com.indraazimi.mobpro2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), MainDialog.DialogListener {
@@ -28,7 +29,7 @@ class MainActivity : AppCompatActivity(), MainDialog.DialogListener {
         }
     }
 
-    override fun processDialog() {
-        Log.d("MainActivity", "Dialog button clicked!")
+    override fun processDialog(mahasiswa: Mahasiswa) {
+        Log.d("MainActivity", mahasiswa.toString())
     }
 }

app/src/main/java/com/indraazimi/mobpro2/MainDialog.kt
@@ -11,9 +11,12 @@ package com.indraazimi.mobpro2
 
 import android.app.Dialog
 import android.os.Bundle
+import android.view.Gravity
 import android.view.LayoutInflater
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
+import com.indraazimi.mobpro2.data.Mahasiswa
 import com.indraazimi.mobpro2.databinding.DialogMainBinding
 
 class MainDialog : DialogFragment() {
@@ -28,15 +31,46 @@ class MainDialog : DialogFragment() {
             setTitle(R.string.tambah_mahasiswa)
             setView(binding.root)
             setPositiveButton(R.string.simpan) { _, _ ->
+                val mahasiswa = getData() ?: return@setPositiveButton
                 val listener = requireActivity() as DialogListener
-                listener.processDialog()
+                listener.processDialog(mahasiswa)
             }
             setNegativeButton(R.string.batal) { _, _ -> dismiss() }
         }
         return builder.create()
     }
 
+    private fun getData(): Mahasiswa? {
+        // Lakukan sanity check terlebih dahulu
+        if (binding.nimEditText.text.isEmpty()) {
+            showMessage(R.string.nim_wajib_diisi)
+            return null
+        }
+
+        if (binding.nimEditText.text.length != 10) {
+            showMessage(R.string.nim_harus_10chars)
+            return null
+        }
+
+        if (binding.namaEditText.text.isEmpty()) {
+            showMessage(R.string.nama_wajib_diisi)
+            return null
+        }
+
+        return Mahasiswa(
+            nim = binding.nimEditText.text.toString(),
+            nama = binding.namaEditText.text.toString()
+        )
+    }
+
+    private fun showMessage(messageResId: Int) {
+        Toast.makeText(requireContext(), messageResId, Toast.LENGTH_LONG).apply {
+            setGravity(Gravity.CENTER, 0, 0)
+            show()
+        }
+    }
+
     interface DialogListener {
-        fun processDialog()
+        fun processDialog(mahasiswa: Mahasiswa)
     }
 }