jonrysimbolon/mahasiswa-id

Menampilkan data mahasiswa ke list

app/src/main/java/com/indraazimi/mahasiswaid/MainAdapter.kt
@@ -1,0 +1,53 @@
+/*
+ * Copyright (c) 2021 Indra Azimi. All rights reserved.
+ *
+ * Dibuat untuk kelas Pemrograman untuk Perangkat Bergerak 2.
+ * Dilarang melakukan penggandaan dan atau komersialisasi,
+ * sebagian atau seluruh bagian, baik cetak maupun elektronik
+ * terhadap project ini tanpa izin pemilik hak cipta.
+ */
+
+package com.indraazimi.mahasiswaid
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.indraazimi.mahasiswaid.data.Mahasiswa
+import com.indraazimi.mahasiswaid.databinding.ItemMainBinding
+
+class MainAdapter : ListAdapter<Mahasiswa, MainAdapter.ViewHolder>(DIFF_CALLBACK) {
+
+    companion object {
+        private val DIFF_CALLBACK = object : DiffUtil.ItemCallback<Mahasiswa>() {
+            override fun areItemsTheSame(oldData: Mahasiswa, newData: Mahasiswa): Boolean {
+                return oldData.id == newData.id
+            }
+
+            override fun areContentsTheSame(oldData: Mahasiswa, newData: Mahasiswa): Boolean {
+                return oldData == newData
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ItemMainBinding.inflate(inflater, parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    inner class ViewHolder(
+        private val binding: ItemMainBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(mahasiswa: Mahasiswa) {
+            binding.nimTextView.text = mahasiswa.nim
+            binding.namaTextView.text = mahasiswa.nama
+        }
+    }
+}

indraazimi/mobpro2

app/src/main/java/com/indraazimi/mobpro2/MainAdapter.kt
@@ -1,0 +1,53 @@
+/*
+ * Copyright (c) 2020-2023 Indra Azimi. All rights reserved.
+ *
+ * Dibuat untuk kelas Pemrograman untuk Perangkat Bergerak 2.
+ * Dilarang melakukan penggandaan dan atau komersialisasi,
+ * sebagian atau seluruh bagian, baik cetak maupun elektronik
+ * terhadap project ini tanpa izin pemilik hak cipta.
+ */
+
+package com.indraazimi.mobpro2
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.indraazimi.mobpro2.data.Mahasiswa
+import com.indraazimi.mobpro2.databinding.ItemMainBinding
+
+class MainAdapter : ListAdapter<Mahasiswa, MainAdapter.ViewHolder>(DIFF_CALLBACK) {
+
+    companion object {
+        private val DIFF_CALLBACK = object : DiffUtil.ItemCallback<Mahasiswa>() {
+            override fun areItemsTheSame(oldData: Mahasiswa, newData: Mahasiswa): Boolean {
+                return oldData.id == newData.id
+            }
+
+            override fun areContentsTheSame(oldData: Mahasiswa, newData: Mahasiswa): Boolean {
+                return oldData == newData
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ItemMainBinding.inflate(inflater, parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    inner class ViewHolder(
+        private val binding: ItemMainBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(mahasiswa: Mahasiswa) {
+            binding.nimTextView.text = mahasiswa.nim
+            binding.namaTextView.text = mahasiswa.nama
+        }
+    }
+}