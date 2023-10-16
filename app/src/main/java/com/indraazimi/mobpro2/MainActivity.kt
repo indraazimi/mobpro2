jonrysimbolon/mahasiswa-id

Menyimpan data mahasiswa ke database

app/src/main/java/com/indraazimi/mahasiswaid/MainActivity.kt
@@ -10,12 +10,19 @@
 package com.indraazimi.mahasiswaid
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import com.indraazimi.mahasiswaid.data.Mahasiswa
+import com.indraazimi.mahasiswaid.data.MahasiswaDb
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), MainDialog.DialogListener {
+
+    private val viewModel: MainViewModel by lazy {
+        val dataSource = MahasiswaDb.getInstance(this).dao
+        val factory = MainViewModelFactory(dataSource)
+        ViewModelProvider(this, factory).get(MainViewModel::class.java)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +34,6 @@ class MainActivity : AppCompatActivity(), MainDialog.DialogListener {
     }
 
     override fun processDialog(mahasiswa: Mahasiswa) {
-        Log.d("MainActivity", mahasiswa.toString())
+        viewModel.insertData(mahasiswa)
     }
 }

app/src/main/java/com/indraazimi/mahasiswaid/MainViewModel.kt
@@ -1,0 +1,29 @@
+/*
+ * Copyright (c) 2020 Indra Azimi. All rights reserved.
+ *
+ *  Dibuat untuk kelas Pemrograman untuk Perangkat Bergerak 2.
+ *  Dilarang melakukan penggandaan dan atau komersialisasi,
+ *  sebagian atau seluruh bagian, baik cetak maupun elektronik
+ *  terhadap project ini tanpa izin pemilik hak cipta.
+ */
+
+package com.indraazimi.mahasiswaid
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.indraazimi.mahasiswaid.data.Mahasiswa
+import com.indraazimi.mahasiswaid.data.MahasiswaDao
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class MainViewModel(private val db : MahasiswaDao) : ViewModel() {
+
+    fun insertData(mahasiswa: Mahasiswa) {
+        viewModelScope.launch {
+            withContext(Dispatchers.IO) {
+                db.insertData(mahasiswa)
+            }
+        }
+    }
+}

app/src/main/java/com/indraazimi/mahasiswaid/data/MahasiswaDb.kt
@@ -1,0 +1,41 @@
+/*
+ * Copyright (c) 2020 Indra Azimi. All rights reserved.
+ *
+ *  Dibuat untuk kelas Pemrograman untuk Perangkat Bergerak 2.
+ *  Dilarang melakukan penggandaan dan atau komersialisasi,
+ *  sebagian atau seluruh bagian, baik cetak maupun elektronik
+ *  terhadap project ini tanpa izin pemilik hak cipta.
+ */
+
+package com.indraazimi.mahasiswaid.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Mahasiswa::class], version = 1, exportSchema = false)
+abstract class MahasiswaDb : RoomDatabase()  {
+
+    abstract val dao : MahasiswaDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: MahasiswaDb? = null
+
+        fun getInstance(context: Context): MahasiswaDb {
+            synchronized(this) {
+                var instance = INSTANCE
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        MahasiswaDb::class.java,
+                        "mahasiswa.db"
+                    ).build()
+                    INSTANCE = instance
+                }
+                return instance
+            }
+        }
+    }
+}

indraazimi/mobpro2

app/src/main/java/com/indraazimi/mobpro2/MainActivity.kt
@@ -10,12 +10,19 @@
 package com.indraazimi.mobpro2
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import com.indraazimi.mobpro2.data.Mahasiswa
+import com.indraazimi.mobpro2.data.MahasiswaDb
 import com.indraazimi.mobpro2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), MainDialog.DialogListener {
+
+    private val viewModel: MainViewModel by lazy {
+        val dataSource = MahasiswaDb.getInstance(this).dao
+        val factory = MainViewModelFactory(dataSource)
+        ViewModelProvider(this, factory)[MainViewModel::class.java]
+    }
 
     private lateinit var binding: ActivityMainBinding
 
@@ -30,6 +37,6 @@ class MainActivity : AppCompatActivity(), MainDialog.DialogListener {
     }
 
     override fun processDialog(mahasiswa: Mahasiswa) {
-        Log.d("MainActivity", mahasiswa.toString())
+        viewModel.insertData(mahasiswa)
     }
 }

app/src/main/java/com/indraazimi/mobpro2/MainViewModel.kt
@@ -1,0 +1,29 @@
+/*
+ * Copyright (c) 2020-2023 Indra Azimi. All rights reserved.
+ *
+ * Dibuat untuk kelas Pemrograman untuk Perangkat Bergerak 2.
+ * Dilarang melakukan penggandaan dan atau komersialisasi,
+ * sebagian atau seluruh bagian, baik cetak maupun elektronik
+ * terhadap project ini tanpa izin pemilik hak cipta.
+ */
+
+package com.indraazimi.mobpro2
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.indraazimi.mobpro2.data.Mahasiswa
+import com.indraazimi.mobpro2.data.MahasiswaDao
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class MainViewModel(private val db : MahasiswaDao) : ViewModel() {
+
+    fun insertData(mahasiswa: Mahasiswa) {
+        viewModelScope.launch {
+            withContext(Dispatchers.IO) {
+                db.insertData(mahasiswa)
+            }
+        }
+    }
+}

app/src/main/java/com/indraazimi/mobpro2/data/MahasiswaDb.kt
@@ -1,0 +1,41 @@
+/*
+ * Copyright (c) 2020-2023 Indra Azimi. All rights reserved.
+ *
+ * Dibuat untuk kelas Pemrograman untuk Perangkat Bergerak 2.
+ * Dilarang melakukan penggandaan dan atau komersialisasi,
+ * sebagian atau seluruh bagian, baik cetak maupun elektronik
+ * terhadap project ini tanpa izin pemilik hak cipta.
+ */
+
+package com.indraazimi.mobpro2.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Mahasiswa::class], version = 1, exportSchema = false)
+abstract class MahasiswaDb : RoomDatabase()  {
+
+    abstract val dao : MahasiswaDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: MahasiswaDb? = null
+
+        fun getInstance(context: Context): MahasiswaDb {
+            synchronized(this) {
+                var instance = INSTANCE
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        MahasiswaDb::class.java,
+                        "mahasiswa.db"
+                    ).build()
+                    INSTANCE = instance
+                }
+                return instance
+            }
+        }
+    }
+}