indraazimi/mahasiswa-id

Membuat fragment kelas

app/src/main/java/com/indraazimi/mahasiswaid/ui/master/KelasFragment.kt
@@ -1,0 +1,40 @@
+/*
+ * Copyright (c) 2021 Indra Azimi. All rights reserved.
+ *
+ * Dibuat untuk kelas Pemrograman untuk Perangkat Bergerak 2.
+ * Dilarang melakukan penggandaan dan atau komersialisasi,
+ * sebagian atau seluruh bagian, baik cetak maupun elektronik
+ * terhadap project ini tanpa izin pemilik hak cipta.
+ */
+
+package com.indraazimi.mahasiswaid.ui.master
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import androidx.fragment.app.Fragment
+import com.indraazimi.mahasiswaid.R
+import com.indraazimi.mahasiswaid.databinding.FragmentKelasBinding
+
+class KelasFragment : Fragment() {
+
+    private lateinit var binding: FragmentKelasBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View {
+        binding = FragmentKelasBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val kelas = resources.getStringArray(R.array.kelas)
+        binding.listView.adapter = ArrayAdapter(requireContext(),
+            android.R.layout.simple_list_item_1, android.R.id.text1, kelas)
+        binding.listView.setOnItemClickListener { _, _, position, _ ->
+            Log.d("KELAS", kelas[position])
+        }
+    }
+}

indraazimi/mobpro2

app/src/main/java/com/indraazimi/mobpro2/ui/master/KelasFragment.kt
@@ -1,0 +1,40 @@
+/*
+ * Copyright (c) 2020-2023 Indra Azimi. All rights reserved.
+ *
+ * Dibuat untuk kelas Pemrograman untuk Perangkat Bergerak 2.
+ * Dilarang melakukan penggandaan dan atau komersialisasi,
+ * sebagian atau seluruh bagian, baik cetak maupun elektronik
+ * terhadap project ini tanpa izin pemilik hak cipta.
+ */
+
+package com.indraazimi.mobpro2.ui.master
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import androidx.fragment.app.Fragment
+import com.indraazimi.mobpro2.R
+import com.indraazimi.mobpro2.databinding.FragmentKelasBinding
+
+class KelasFragment : Fragment() {
+
+    private lateinit var binding: FragmentKelasBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View {
+        binding = FragmentKelasBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val kelas = resources.getStringArray(R.array.kelas)
+        binding.listView.adapter = ArrayAdapter(requireContext(),
+            android.R.layout.simple_list_item_1, android.R.id.text1, kelas)
+        binding.listView.setOnItemClickListener { _, _, position, _ ->
+            Log.d("KELAS", kelas[position])
+        }
+    }
+}