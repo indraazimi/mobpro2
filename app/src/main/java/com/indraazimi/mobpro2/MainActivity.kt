4e83e566f594621d8c1f63935534acfd8c2ccc46
Indra Azimi <indraazimi@yahoo.com>
2023-12-01 16:12:56 +0700

    Meminta izin kamera ke pengguna

--- app/src/main/java/com/indraazimi/mobpro2/MainActivity.kt
+++ app/src/main/java/com/indraazimi/mobpro2/MainActivity.kt
@@ -9,11 +9,22 @@
 
 package com.indraazimi.mobpro2
 
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.indraazimi.mobpro2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val REQUEST_CODE_PERMISSIONS = 1
+        private val REQUIRED_PERMISSIONS = arrayOf(android.Manifest.permission.CAMERA)
+    }
 
     private lateinit var binding: ActivityMainBinding
 
@@ -21,5 +32,38 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        if (allPermissionsGranted()) {
+            startCamera()
+        }
+        else {
+            requestPermission()
+            binding.mulaiButton.setOnClickListener { requestPermission() }
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>,
+                                            grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == REQUEST_CODE_PERMISSIONS && allPermissionsGranted()) {
+            startCamera()
+            binding.mulaiButton.visibility = View.GONE
+        } else {
+            Toast.makeText(this, R.string.tidak_dapat_izin, Toast.LENGTH_LONG).show()
+            binding.mulaiButton.visibility = View.VISIBLE
+        }
+    }
+
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun startCamera() {
+        Log.d("KAMERA", "Izin kamera telah diberikan.")
     }
 }
